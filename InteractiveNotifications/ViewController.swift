b38d0f8a383d88119510aecaab0ffa409d9f235c
Austin Tooley <tooley7@gmail.com>
2018-09-09 09:19:18 -0500

    Initial Commit

---
+++ InteractiveNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InteractiveNotifications
+//
+//  Created by Austin Tooley on 9/9/18.
+//  Copyright © 2018 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
